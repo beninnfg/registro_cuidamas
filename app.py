1feb02fc2defa01f7bbe234c0654691f6a6bb88f
Ana Isabel <anaiasabelfernandezgallardo@gmail.com>
2025-07-10 17:03:08 -0500

    Fix: puerto para Render

--- app.py
+++ app.py
@@ -150,4 +150,5 @@ def ver_paciente(id):
     return render_template("paciente.html", datos=datos)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
